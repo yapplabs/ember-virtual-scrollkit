d64bac1a764840c5118737c7c1b30f9ea597288d
Luke Melia <luke@lukemelia.com>
2016-04-21 17:22:38 -0400

    Emit client-size-change action

--- addon/components/ember-virtual-collection.js
+++ addon/components/ember-virtual-collection.js
@@ -1,10 +1,17 @@
 import EmberCollection from 'ember-collection/components/ember-collection';
 import layout from './ember-virtual-collection/template';
+import Ember from 'ember';
+
+const { set } = Ember;
 
 export default EmberCollection.extend({
   layout: layout,
   classNames: ['ember-virtual-collection'],
   isTouching: false,
+  didInitAttrs(){
+    this._super(...arguments);
+    this._clientSizeChange = this.getAttr('client-size-change');
+  },
   actions: {
     touchingChange(val){
       this.set('isTouching', val);
@@ -22,6 +29,17 @@ export default EmberCollection.extend({
           set(this, '_scrollTop', scrollTop);
           this._needsRevalidate();
         }
+      }
+    },
+    clientSizeChange(clientWidth, clientHeight) {
+      if (this._clientSizeChange) {
+        this.sendAction('client-size-change', clientWidth, clientHeight);
+      }
+      if (this._clientWidth !== clientWidth ||
+          this._clientHeight !== clientHeight) {
+        set(this, '_clientWidth', clientWidth);
+        set(this, '_clientHeight', clientHeight);
+        this._needsRevalidate();
       }
     }
   }
