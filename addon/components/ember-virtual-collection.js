7979a06e8773728a4b05ad82bd83d52133b80681
Luke Melia <luke@lukemelia.com>
2016-04-27 23:49:53 -0400

    capture clicks when scrolling or dragging

--- addon/components/ember-virtual-collection.js
+++ addon/components/ember-virtual-collection.js
@@ -8,9 +8,46 @@ export default EmberCollection.extend({
   layout: layout,
   classNames: ['ember-virtual-collection'],
   isTouching: false,
+  captureClicksWhenScrolling: true,
   didInitAttrs(){
     this._super(...arguments);
     this._clientSizeChange = this.getAttr('client-size-change');
+  },
+  didInsertElement(){
+    this._super(...arguments);
+    if (this.get('captureClicksWhenScrolling')) {
+      this.setupClickCapture();
+    }
+  },
+  willDestroyElement(){
+    this._super(...arguments);
+    if (this.get('captureClicksWhenScrolling')) {
+      this.teardownClickCapture();
+    }
+  },
+  setupClickCapture(){
+    let component = this;
+    let element = this.get('element');
+    component.captureClick = function(e){
+      e.stopPropagation(); // Stop the click from being propagated.
+      this.removeEventListener('click', component.captureClick, true); // cleanup
+    };
+    component.ccMousedown = function() {
+      component._didScroll = false;
+    };
+    component.ccMouseup = function() {
+      if (component._didScroll || (component._isScrolling && Math.abs(component._decelerationVelocityY) > 2)) {
+        element.addEventListener('click', component.captureClick, true);
+      }
+    };
+    element.addEventListener('mousedown', component.ccMousedown, false);
+    element.addEventListener('mouseup', component.ccMouseup, false);
+  },
+  teardownClickCapture(){
+    let element = this.get('element');
+    element.removeEventListener('click', this.captureClick, true);
+    element.removeEventListener('mousedown', this.ccMousedown, false);
+    element.removeEventListener('mouseup', this.ccMouseup, false);
   },
   actions: {
     touchingChange(val){
@@ -18,6 +55,10 @@ export default EmberCollection.extend({
     },
     scrollingComplete() {
       this.sendAction('scrolling-complete');
+      setTimeout(()=>{
+        this._isScrolling = false;
+        this._decelerationVelocityY = 0;
+      }, 0);
     },
     scrollChange(scrollLeft, scrollTop, params) {
       if (this._scrollChange) {
@@ -30,6 +71,8 @@ export default EmberCollection.extend({
           this._needsRevalidate();
         }
       }
+      this._isScrolling = this._didScroll = true;
+      this._decelerationVelocityY = params.decelerationVelocityY;
     },
     clientSizeChange(clientWidth, clientHeight) {
       if (this._clientSizeChange) {
